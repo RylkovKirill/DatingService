e541694b8dbf115fc9c2aeff0f1688dd8cdea6c6
vladf <vlad.flf@yandex.ru>
2021-11-11 15:44:34 +0300

    new_card

---
+++ DatingService.Infrastructure/ViewModels/CardsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingService.Infrastructure.ViewModels
+{
+    public class CardsViewModel
+    {
+        public string image { get; set; }
+        public string userText { get; set; }
+
+    }
+}
