d30c3bfaef5e25c083ba8bd10ed023c514b9fc12
user <user@LAPTOP-2QKRESKA>
2021-11-23 02:31:21 +0300

    Change Layout

--- DatingService/wwwroot/js/site.js
+++ DatingService/wwwroot/js/site.js
@@ -2,3 +2,8 @@
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
+
+function changeMenu() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
